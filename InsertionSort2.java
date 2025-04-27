db56cff247a343864af67160e9769413ab7668ff
Yugank Tripathi <mr.yugank.2000@gmail.com>
2025-04-27 18:03:14 +0530

    Add insertionsort2 file

--- InsertionSort2.java
+++ InsertionSort2.java
@@ -1,30 +1,30 @@
 import java.util.*;
 
-public class BubbleSort {
+public class InsertionSort2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of Element:");
+        System.out.print("Enter number of Elements:");
         int n = sc.nextInt();
-        int[] arr = new int[n];
         System.out.print("Enter Elements:");
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        for (int i = n-1; i >= 0; i--) {
-            for (int j = 0 ; j < i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+        for (int i = 0; i < n; i++) {
+            int j = i;
+            while (j>0 && arr[j-1] > arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+
+                j--;
             }
         }
+
         for (int i = 0; i < n; i++) {
-            
             System.out.print(arr[i] + " ");
         }
-
         sc.close();
     }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,35 +1,34 @@
 import java.util.*;
+
 public class SelectionSort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of element:");
+        System.out.print("Enter number of element: ");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        System.out.print("Enter Elements:");
+        System.out.print("Enter Elements: ");
         for (int i = 0; i < n; i++) {
             int num = sc.nextInt();
             arr[i] = num;
-            
+
         }
-        
-        for (int i = 0; i < n-1; i++) {
+
+        for (int i = 0; i < n - 1; i++) {
             int index = i;
             for (int j = i; j < n; j++) {
                 if (arr[j] < arr[index]) {
                     index = j;
                 }
             }
-        int temp = arr[i];
-        arr[i] = arr[index];
-        arr[index] = temp;
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
         }
 
         for (int i = 0; i < n; i++) {
-            
+
             System.out.print(arr[i] + " ");
         }
-        
-
 
         sc.close();
     }
